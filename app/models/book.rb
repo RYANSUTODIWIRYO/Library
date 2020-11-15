d65e9e3c46d7dd32cd23083f9e42346282455631
RYANSUTODIWIRYO <ryan.s.sutodiwiryo@gmail.com>
2020-11-16 00:08:55 +0700

    update controller and view

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,5 +1,5 @@
 class Book < ApplicationRecord
     belongs_to :subject
     validates_presence_of :title # title must not be null
-    validates_numericalitu_of :price, :messsage=>"Error Message" # price should be numeric
+    validates_numericality_of :price, :messsage=>"Error Message" # price should be numeric
 end
